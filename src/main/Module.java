23058d0cf234b3bba2aba229ac40484d58bbb935
Prachi Javeri <psj2108@columbia.edu>
2012-10-31 12:41:46 -0400

    Getting all the Links

--- src/main/Module.java
+++ src/main/Module.java
@@ -3,15 +3,11 @@ package main;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.LinkedList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Module 
 {
@@ -25,35 +21,28 @@ public class Module
 			course_urls.add(s);
 		br.close();
 		System.out.println("---------------------------------------------------");
-		
+		System.out.println();
 		//GETTING CONTENT OF EACH OF THE URLS 
 		for(int i=0;i<course_urls.size();i++)
 		{
+			System.out.println(course_urls.get(i).toUpperCase());
 			Document doc = Jsoup.connect(course_urls.get(i)).get();
-		    String text = doc.body().text(); 
+		    String text = doc.body().text();
+		    Elements links = doc.select("a");
+		    for(int j=0;j<links.size();j++)
+		    {
+		    	//System.out.println(links.get(j).text());
+		    	System.out.println(links.get(j).attr("abs:href"));
+		    }
+		    System.out.println();
+			System.out.println("---------------------------------------------------");
+			System.out.println();
 		    System.out.println("\n\n\n\n");
 		    String[] splitString = text.split("(textbook|Textbooks)");
 		    for(int j = 0; j < splitString.length ; j ++)
 		    {
 		        System.out.println(splitString[j]+"\n"+"\n");
 		    }
-
-//			Document doc = Jsoup.connect(course_urls.get(i)).get();
-//			Elements body=doc.select(body);
-//			System.out.println(course_urls.get(i).toUpperCase());
-//			URL url = new URL(course_urls.get(i));
-//			InputStream is = url.openConnection().getInputStream();
-//			BufferedReader reader = new BufferedReader( new InputStreamReader( is )  );
-//	        String line = "";
-//			String html="";
-//	        while( ( line = reader.readLine() ) != null )  
-//	        {
-//	        	html+=(line+"\n");
-//	        }
-//			Document doc = Jsoup.parseBodyFragment(html);
-//			Element body = doc.body();
-//			System.out.println(body.text());
-//		    reader.close();
 		}
 	}
 }
